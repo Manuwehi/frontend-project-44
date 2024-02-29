c05b1af61f77a306887bd3f515d64dade891e8bb
Alex <manuwehial@gmail.com>
2024-02-29 22:09:48 +0500

    calc comm

--- games/calc-game.js
+++ games/calc-game.js
@@ -2,15 +2,15 @@ import readlineSync from 'readline-sync';
 
 const calcGame = () => {
   const textQa = [];
-  const operandArr = ['+', '-', '*'];
-  const randomOperand = operandArr[Math.floor(Math.random() * (operandArr.length-1))];
+  const operatorArr = ['+', '-', '*'];
+  const randomOperator = operatorArr[Math.floor(Math.random() * (operandArr.length-1))];
   const leftRandomNum = Math.floor(Math.random() * 100 + 1);
   const rightRandomNum = Math.floor(Math.random() * 100 + 1);
   let count = 0;
   const maxCount = 3;
   while (count < maxCount) {
-    const rightAnswer = leftRandomNum randomOperand rightRandomNum;
-    const userAnswer = readlineSync.question(`Question: ${leftRandomNum} ${randomOperand} ${rightRandomNum} \nYour answer: `);
+    const rightAnswer = leftRandomNum randomOperator rightRandomNum; // Вот тут ошибка
+    const userAnswer = readlineSync.question(`Question: ${leftRandomNum} ${randomOperator} ${rightRandomNum} \nYour answer: `);
     textQa.push()
     count +=1;
   }
