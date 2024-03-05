617bcfcc820179c0fa020714e75283dbbc193114
Alex <manuwehial@gmail.com>
2024-03-05 20:25:49 +0500

    Revert "Optimized prime-game"
    This reverts commit f68e8c235e72c88c0fefe232152d4b208aedf1d5.

--- games/prime-game.js
+++ games/prime-game.js
@@ -4,7 +4,7 @@ const primeGame = () => {
   const message = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   const num = getRandomNum();
   let rightAnswer = true;
-  for (let i = 2; i < Math.floor(Math.sqrt(num)); i += 1) {
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
       rightAnswer = false;
     }
