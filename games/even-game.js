5eb80d0719c1b650cbd1cd4875db78c38db5ba59
Alex <manuwehial@gmail.com>
2024-03-05 15:35:44 +0500

    Changed even-game directory

---
+++ games/even-game.js
@@ -0,0 +1,11 @@
+import getRandomNum from '../src/getRandomNum.js';
+
+const isEvenGame = () => {
+  const message = 'Answer "yes" if the number is even, otherwise answer "no"';
+  const randomNum = getRandomNum();
+  const rightAnswer = !(randomNum % 2) ? 'yes' : 'no';
+  const finalArr = [message, randomNum, rightAnswer];
+  return finalArr;
+};
+
+export default isEvenGame;
