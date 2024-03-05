fed6b488b9fe27bfa0eced0f4703eef45b031968
Alex <manuwehial@gmail.com>
2024-03-05 20:51:01 +0500

    maybe fixed progression

--- games/progression-game.js
+++ games/progression-game.js
@@ -3,12 +3,12 @@ import getRandomNum from '../src/getRandomNum.js';
 const progGame = () => {
   const message = 'What number is missing in the progression?';
   const arrLength = Math.floor(Math.random() * 10 + 5); // 5 - мин длина массива, 10 - макс
-  const numArr = [];
+  let numArr = [];
   const num = getRandomNum();
   let temp = num;
   const randIndex = Math.floor(Math.random() * arrLength);
   while (numArr.length < arrLength) {
-    numArr.push(temp);
+    numArr = numArr.concat(temp);
     temp += num;
   }
   const fakeArr = numArr;
